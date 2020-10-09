cd6ddec2e9748f616ac03f5f45b995ae63b0491f
Mohamed Fathy <codetwostart@gmail.com>
2020-10-09 18:18:44 +0200

    lesson 5

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -7,6 +9,8 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  var myMarkers = HashSet<Marker>(); //collection
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,9 +20,19 @@ class _HomeState extends State<Home> {
         body: Stack(
           children: [
             GoogleMap(
-              initialCameraPosition: CameraPosition(
-                  target: LatLng(37.43296265331129, -122.08832357078792),
-                  zoom: 14),
+              initialCameraPosition:
+                  CameraPosition(target: LatLng(30.0444, 31.2357), zoom: 14),
+              onMapCreated: (GoogleMapController googleMapController) {
+                setState(() {
+                  myMarkers.add(
+                    Marker(
+                      markerId: MarkerId('1'),
+                      position: LatLng(30.0444, 31.2357),
+                    ),
+                  );
+                });
+              },
+              markers: myMarkers,
             ),
             Container(
               child: Image.asset('assets/images/c2s.png'),
