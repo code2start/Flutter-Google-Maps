3ac1b7d309abc7fa0c6550c5a7cf12cb8c59479f
Mohamed Fathy <codetwostart@gmail.com>
2020-10-09 06:13:26 +0200

    Lesson 3

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,13 +10,21 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('First Google Map'),
-      ),
-      body: GoogleMap(
-        initialCameraPosition: CameraPosition(
-            target: LatLng(37.43296265331129, -122.08832357078792), zoom: 19),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('First Google Map'),
+        ),
+        body: Stack(
+          children: [
+            GoogleMap(
+              initialCameraPosition: CameraPosition(
+                  target: LatLng(37.43296265331129, -122.08832357078792),
+                  zoom: 14),
+            ),
+            Container(
+              child: Image.asset('assets/images/c2s.png'),
+              alignment: Alignment.topCenter,
+            )
+          ],
+        ));
   }
 }
